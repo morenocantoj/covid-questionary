0c61663b7f649f3f7c0cb96d88ea811d1d27ffcb
Jaume Moreno <morenocantoj@gmail.com>
2020-03-15 12:55:15 +0100

    Basic BBDD settings and view

--- covid_advisor/covid_advisor/settings.py
+++ covid_advisor/covid_advisor/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrap4',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'covid_advisor.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'covid_advisor.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'covid_advisor',
+        'USER': 'covid_advisor_user',
+        'PASSWORD': 'helping_people',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
